bc857bc033bd280b617e434b89d609bc1b26285c
hsync <felixschulze@hotmail.de>
2014-03-10 21:08:19 +0100

    Update 10.03.2014

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,54 @@
 #include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #include "config.h"
 #include "connection.h"
 #include "modus.h"
 
 using namespace std;
+volatile int run = true;
 
+void my_handler(int x)
+{
+	if(x==2)
+		run = false;
+}
 
 int main()
 {
+	signal(SIGINT, my_handler);
+	
 	config configuration;
 	modus mode;
 	connection artNet(configuration.getIp(), configuration.getPort());
 	int wahl = 0;
 
-	cout << "Sende Packete an: " << configuration.getIp() << ":" << configuration.getPort();
-	cout << "\nModus auswählen\n";
-	cout << "<1> Manuell\n";
-	cout << "<2> RGB-Fade\n";
-	cout << "\nBitte wählen: ";
-	cin >> wahl;
+	cout << "Art-Net packets to: " << configuration.getIp() << ":" << configuration.getPort();
 
-	switch(wahl)
+	
+	while(1)
 	{
-		case 1:	mode.manualMode(artNet);
-			break;
+		cout << "\033[2J\033[1;1H";
+		cout << "choose mode\n";
+		cout << "<1> manuell\n";
+		cout << "<2> rgb-fade\n";
+		cout << "<3> exit\n";
+		cout << "\n>> ";
+		cin >> wahl;
+		run = true;
+		switch(wahl)
+		{
+			case 1:	mode.manualMode(artNet);
+				break;
 
-		case 2: mode.rgbMode(artNet, configuration);
-			break;
+			case 2: mode.rgbMode(artNet, configuration);
+				break;
+			
+			case 3: exit(1);
+		}
 	}
 }
--- src/modus.cpp
+++ src/modus.cpp
@@ -3,10 +3,13 @@
 
 using namespace std;
 
+extern volatile int run;
+
 void modus::manualMode(connection x)
 {
 	int channel;
 	int value;
+	char buffer[10];
 	while(1)
 	{
 		cout << "\033[2J";
@@ -14,35 +17,31 @@ void modus::manualMode(connection x)
 		{
 			printf("\033[%d;1HCh: %d", i, i);
 			printf("\033[%d;9HValue: %d", i, x.getValue(i));
+			printf("\033[%d;20HCh: %d", i, i+20);
+            printf("\033[%d;28HValue: %d", i, x.getValue(i+20));
+            printf("\033[%d;39HCh: %d", i, i+40);
+            printf("\033[%d;47HValue: %d", i, x.getValue(i+40));
+			printf("\033[%d;58HCh: %d", i, i+60);
+            printf("\033[%d;66HValue: %d", i, x.getValue(i+60));
+            printf("\033[%d;77HCh: %d", i, i+80);
+            printf("\033[%d;85HValue: %d", i, x.getValue(+80));
 		}
-                for(int i = 21; i <= 40; i++)
-                {
-                        printf("\033[%d;20HCh: %d", i-20, i);
-                        printf("\033[%d;28HValue: %d", i-20, x.getValue(i));
-                }
-                for(int i = 41; i <= 60; i++)
-                {
-                        printf("\033[%d;39HCh: %d", i-40, i);
-                        printf("\033[%d;47HValue: %d", i-40, x.getValue(i));
-                }
-                for(int i = 61; i <= 80; i++)
-                {
-                        printf("\033[%d;58HCh: %d", i-60, i);
-                        printf("\033[%d;66HValue: %d", i-60, x.getValue(i));
-                }
-                for(int i = 81; i <= 100; i++)
-                {
-                        printf("\033[%d;77HCh: %d", i-80, i);
-                        printf("\033[%d;85HValue: %d", i-80, x.getValue(i));
-                }
+		
+		cout << "\n\nc[channel] 	--> set channel";
+		cout << "\nv[value] 	--> set value";
+		cout << "\nq		--> quit mode";
 
+		cout << "\n\n>> ";
+		cin >> buffer;
+		if(buffer[0] == 'c')
+			channel = atoi(&buffer[1]);	
+		if(buffer[0] == 'v')
+			value = atoi(&buffer[1]);
+		if(buffer[0] == 'q')
+			break;
+			
 
-
-
-		cout << "\n\nKanal : ";
-		cin >> channel;
-		cout << "Wert  : ";
-		cin >> value;
+		
 		x.setValue(channel, value);
 		x.sendPackage();
 	}
@@ -56,13 +55,15 @@ void modus::rgbMode(connection x, config y)
 	int blau = 0;
 
 	cout << "\033[2J\033[1;1H";
-	cout << "RGB-Fade";
-	cout << "\nRot   Channel: " <<y.getChRot();
-	cout << "\nGruen Channel: " <<y.getChGruen();
-	cout << "\nBlau  Channel: " <<y.getChBlau() << "\n";
-
-	while(1)
+	cout << "rgb-fade";
+	cout << "\nred   channel: " <<y.getChRot();
+	cout << "\ngreen channel: " <<y.getChGruen();
+	cout << "\nblue  channel: " <<y.getChBlau() << "\n";
+	
+	while(run)
 	{
+		
+		
 		if(mode == 0){rot --; gruen ++;}
 		if(mode == 1){gruen --; blau ++;}
 		if(mode == 2){blau --; rot ++;}
